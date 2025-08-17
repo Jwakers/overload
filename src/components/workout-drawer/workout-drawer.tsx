7b13173574e35e80ca8d08632d66346b2a28f2ec
Jack Wakeham <jackwakeham82@gmail.com>
2025-08-17 16:08:49 +0100

    feat: add the set form

--- src/components/workout-drawer/workout-drawer.tsx
+++ src/components/workout-drawer/workout-drawer.tsx
@@ -2,14 +2,20 @@ import { cn } from "@/lib/utils";
 import { useMutation, useQuery } from "convex/react";
 import {
   CheckIcon,
-  Delete,
   Filter as FilterIcon,
   Plus,
   PlusCircle,
   Save,
+  Trash2,
   X,
 } from "lucide-react";
-import { Dispatch, SetStateAction, useEffect, useState } from "react";
+import {
+  Dispatch,
+  SetStateAction,
+  useEffect,
+  useState,
+  useTransition,
+} from "react";
 import { api } from "../../../convex/_generated/api";
 import { Id } from "../../../convex/_generated/dataModel";
 import {
@@ -37,7 +43,63 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "../ui/drawer";
+import { Input } from "../ui/input";
+import { Label } from "../ui/label";
 import { Popover, PopoverContent, PopoverTrigger } from "../ui/popover";
+import { ScrollArea } from "../ui/scroll-area";
+import { Separator } from "../ui/separator";
+import { Textarea } from "../ui/textarea";
+
+// TODO: Handle deleting an exercise set
+
+function WeightUnitToggle() {
+  const user = useQuery(api.users.current);
+  const updatePreferences = useMutation(api.users.updatePreferences);
+  const [updating, startUpdating] = useTransition();
+
+  const handleWeightUnitChange = async (unit: "lbs" | "kg") => {
+    startUpdating(async () => {
+      await updatePreferences({
+        preferences: {
+          defaultWeightUnit: unit,
+        },
+      });
+    });
+  };
+
+  if (!user) return null;
+
+  return (
+    <div className="grid bg-muted grid-cols-2 rounded overflow-hidden">
+      <button
+        className={cn(
+          "p-2 z-10 rounded",
+          updating && "bg-brand/50 cursor-not-allowed",
+          user.preferences?.defaultWeightUnit === "lbs" &&
+            "bg-brand text-brand-foreground"
+        )}
+        onClick={() => handleWeightUnitChange("lbs")}
+        disabled={updating}
+        title="Change weight unit to lbs"
+      >
+        LBS
+      </button>
+      <button
+        className={cn(
+          "p-2 z-10 rounded transition-colors",
+          updating && "bg-brand/50 cursor-not-allowed",
+          user.preferences?.defaultWeightUnit === "kg" &&
+            "bg-brand text-brand-foreground"
+        )}
+        onClick={() => handleWeightUnitChange("kg")}
+        disabled={updating}
+        title="Change weight unit to kg"
+      >
+        KG
+      </button>
+    </div>
+  );
+}
 
 export function WorkoutDrawer() {
   const [open, setOpen] = useState(false);
@@ -100,47 +162,61 @@ export function WorkoutDrawer() {
           </button>
         </DrawerTrigger>
         <DrawerContent className="h-full">
-          <DrawerHeader>
+          <DrawerHeader className="space-y-2">
             <DrawerTitle>New Workout</DrawerTitle>
-            <DrawerDescription>Description goes here</DrawerDescription>
+            <WeightUnitToggle />
           </DrawerHeader>
-          <div className="container">
-            <p className="my-2 text-sm text-destructive-foreground bg-destructive p-2 px-4 rounded-md">
-              TODO: There needs to be an way to add a split to this without
-              slowing down the flow.
-            </p>
-          </div>
-          <div className="container flex flex-col gap-2 h-full justify-end">
-            {exerciseSets?.map((exerciseSet) => (
-              <div className="border p-4" key={exerciseSet._id}>
-                <p className="font-semibold">{exerciseSet.exercise?.name}</p>
-              </div>
-            ))}
-            <button
-              className={cn(
-                "flex justify-between gap-2 rounded border border-dashed p-4",
-                !workoutSessionId && "opacity-50 cursor-not-allowed"
-              )}
-              onClick={() => setSelectExerciseDialogOpen(true)}
-              disabled={!workoutSessionId}
-            >
-              <p className="font-semibold">Add first exercise</p>
-              <PlusCircle className="text-brand" size={24} />
-            </button>
-          </div>
+          <ScrollArea className="h-full overflow-y-auto">
+            <div className="container">
+              <p className="my-2 text-sm text-destructive-foreground bg-destructive p-2 px-4 rounded-md">
+                TODO: There needs to be an way to add a split to this without
+                slowing down the flow.
+              </p>
+            </div>
+            <div className="container flex flex-col gap-2 h-full justify-end">
+              {exerciseSets?.map((exerciseSet) => (
+                <div className="border p-4 space-y-4" key={exerciseSet._id}>
+                  <div className="gap-2 flex justify-between items-center">
+                    <p className="font-semibold">
+                      {exerciseSet.exercise?.name}
+                    </p>
+                    <button title="Delete exercise">
+                      <Trash2 className="text-destructive" />
+                    </button>
+                  </div>
+                  <ExerciseSetForm />
+                  <Separator />
+                  <div className="flex gap-2 justify-between">
+                    <Button variant="outline">Finish exercise</Button>
+                    <Button variant="default">Save and start next set</Button>
+                  </div>
+                </div>
+              ))}
+              <button
+                className={cn(
+                  "flex justify-between gap-2 rounded border border-dashed p-4",
+                  !workoutSessionId && "opacity-50 cursor-not-allowed"
+                )}
+                onClick={() => setSelectExerciseDialogOpen(true)}
+                disabled={!workoutSessionId}
+              >
+                <p className="font-semibold">
+                  {exerciseSets?.length ? "Add exercise" : "Add first exercise"}
+                </p>
+                <PlusCircle className="text-brand" size={24} />
+              </button>
+            </div>
 
-          <SelectExerciseDrawer
-            open={selectExerciseDialogOpen}
-            onChange={setSelectExerciseDialogOpen}
-            onSelect={handleSelectExercise}
-          />
+            <SelectExerciseDrawer
+              open={selectExerciseDialogOpen}
+              onChange={setSelectExerciseDialogOpen}
+              onSelect={handleSelectExercise}
+            />
+          </ScrollArea>
 
           <DrawerFooter className="flex flex-row gap-2 max-w-xl mx-auto w-full">
             <DrawerClose asChild>
-              <Button variant="outline">
-                <Delete />
-                Cancel
-              </Button>
+              <Button variant="outline">Cancel</Button>
             </DrawerClose>
             <Button className="grow" variant="primary">
               <Save />
@@ -257,7 +333,7 @@ function SelectExerciseDrawer(props: {
   );
 }
 
-export function ExerciseFilter(props: {
+function ExerciseFilter(props: {
   value: string;
   setValue: (value: string) => void;
 }) {
@@ -310,5 +386,49 @@ export function ExerciseFilter(props: {
         </Command>
       </PopoverContent>
     </Popover>
+  );
+}
+
+function ExerciseSetForm() {
+  // TODO: Submit the form to the exercise set when all fields are filled. Use zod for validation and error handling.
+  // TODO: Pre-populate weight default value with the last exercise set weight. Or the last set weight of the same exercise.
+  const user = useQuery(api.users.current);
+  return (
+    <form>
+      <div className="grid grid-cols-[auto_1fr_1fr] gap-2">
+        <div className="flex flex-col gap-2">
+          <p className="text-xs text-muted-foreground">Set</p>
+          <p className=" text-lg font-semibold grow flex items-center">1</p>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label className="text-xs text-muted-foreground" htmlFor="weight">
+            Weight
+          </Label>
+          <div className="relative">
+            <span className="absolute right-4 border-l pl-2 top-1/2 -translate-y-1/2 text-muted-foreground text-sm">
+              {user?.preferences?.defaultWeightUnit.toLowerCase()}
+            </span>
+            <Input type="number" min={0} id="weight" />
+          </div>
+        </div>
+        <div className="flex flex-col gap-2">
+          <Label className="text-xs text-muted-foreground" htmlFor="reps">
+            Reps
+          </Label>
+          <Input type="number" min={0} id="reps" />
+        </div>
+        <div className="flex flex-col gap-2 col-start-2 col-span-2">
+          <Label className="sr-only" htmlFor="notes">
+            Notes
+          </Label>
+          <Textarea
+            placeholder="Notes"
+            maxLength={255}
+            id="notes"
+            className="resize-none"
+          />
+        </div>
+      </div>
+    </form>
   );
 }
